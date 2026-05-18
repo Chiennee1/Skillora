7418e6ed1c3a4a66abcfc0c6a4d9963b64259cb4
Chiennee1 <23a1001d0048@students.hou.edu.vn>
2026-05-18 17:51:22 +0700

    first commit

---
+++ skillora_platform/src/main/java/com/example/skillora_platform/SkilloraPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.skillora_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkilloraPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkilloraPlatformApplication.class, args);
+	}
+
+}
---
+++ skillora_platform/src/test/java/com/example/skillora_platform/SkilloraPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.skillora_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkilloraPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
